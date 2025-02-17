ab9d552f880dc33022d34195e048a6020236add0
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-17 16:05:31 +0100

    Task:1.The last digit, File => 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 
 	if (ld > 5)
 	{
-		printf("Last digit of %d is %d and is great than 5\n", n, ld);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
 	else if (ld == 0)
 	{
