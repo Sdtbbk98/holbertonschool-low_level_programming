d31c85e8ecc8068f89fd46775777cd2cbe8c18c8
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-17 17:05:55 +0100

    Task: 2. I sometimes suffer from insomnia. And when I can not fall asleep, I play what I call the alphabet game, file: 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,13 @@
 int main(void)
 {
 	char character;
-	for (character = 'a'; character <='z'; character++)
+
+	for (character = 'a'; character <= 'z'; character++)
+
 		putchar(character);
+
 		putchar('\n');
+
 	return (0);
 
 }
