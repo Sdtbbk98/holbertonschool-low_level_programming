1cd7e854a0c839687bac4a300e71a3cdf73e2e0d
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-17 16:59:13 +0100

    Task: 2. I sometimes suffer from insomnia. And when I can not fall asleep, I play what I call the alphabet game, file: 2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -entry level
+ *
+ * Return: it print the alphabet in lowercase
+ */
+int main(void)
+{
+	char character;
+	for (character = 'a'; character <='z'; character++)
+		putchar(character);
+		putchar('\n');
+	return (0);
+
+}
+
