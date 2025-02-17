4e79c6ba435d0c17bcad44ab358c44c9ced719e1
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-17 16:02:44 +0100

    Task:1.The last digit, File => 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,27 +13,26 @@ int main(void)
 {
 
 	int n;
-	int last_digit_of_n;
+	int ld;
 
 
 
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
-	/* the below command will take n random number and divided by 10 and rest is our module*/ 
-	last_digit_of_n = n % 10;
+	ld = n % 10;
 
-	if (last_digit_of_n > 5)
+	if (ld > 5)
 	{
-		printf("Last digit of %d is %d and is great than 5\n", n, last_digit_of_n);
+		printf("Last digit of %d is %d and is great than 5\n", n, ld);
 	}
-	else if (last_digit_of_n == 0)
+	else if (ld == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit_of_n);
+		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit_of_n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	}
 
 
