0652155bbf5d2542b0f73e9fee2dc76a8b94cdd0
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-19 15:47:11 +0100

    Task: 0._putchar, File: 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main -entry level
+ *
+ * Return: value 0
+ */
 
-int main()
+int main(void)
 {
-	char* mots = "_putchar";
+	char *mots = "_putchar";
 
 	printf("%s\n", mots);
 	return (0);
 }
-
 
