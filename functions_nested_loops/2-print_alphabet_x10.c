d677ead3a0ecfebb809d3357eb6feb4609034604
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-21 00:56:07 +0100

    Task:  2. 10 x alphabet, file: 2-print_alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -it will print the alphabet 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int number_of_line = 0;
+	char alphabet;
+
+	while (number_of_line <= 10)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+
+		number_of_line++;
+		_putchar('\n');
+
+	}
+}
