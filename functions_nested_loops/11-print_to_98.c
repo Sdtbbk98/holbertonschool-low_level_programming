2bfe859c4925db8116983c38cb971ed8e8ae97e9
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-22 14:55:13 +0100

    Task. 11, file 11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 -function that print from @n to 98
+ * @n: given number
+ * l_number - int last_number = n(given number)
+ * d_number - int display number till n
+ * Return: void nada
+ */
+
+void print_to_98(int n)
+{
+	int l_number = 98;
+	int d_number;
+
+	if (n < l_number)
+	{
+		for (d_number = n; d_number <= l_number; d_number++)
+		{
+			if (d_number != 98)
+			{
+				printf("%d, ", d_number);
+			} else
+			{
+				printf("%d\n", d_number);
+			}
+		}
+	} else if (n > l_number)
+	{
+		for (d_number = n; d_number > l_number; d_number--)
+		{
+			printf("%d, ", d_number);
+		}
+	} else
+	{
+		printf("%d\n", l_number);
+	}
+
+}
+
