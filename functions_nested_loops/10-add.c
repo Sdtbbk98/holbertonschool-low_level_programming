253fe331f59e016b088c32d37cf280a6b6591a73
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-22 13:19:48 +0100

    Task: 10. a + b, file: 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add -(function)sum the 2 variable
+ *
+ * @num1: int variable
+ * @num2: int variable
+ *
+ *@_sum: calaculate the sum of 2 variable
+ *
+ * Return: @sume
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+	sum = num1 + num2;
+	return (sum);
+}
+
