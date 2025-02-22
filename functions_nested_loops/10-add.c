0c6a725201e24a1f5774a8220c0725c527faf86a
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-22 13:21:45 +0100

    Task: 10. a + b, file: 10-add.c, change: betty correction

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -6,7 +6,7 @@
  * @num1: int variable
  * @num2: int variable
  *
- *@_sum: calaculate the sum of 2 variable
+ * sum -  calaculate the sum of 2 variable
  *
  * Return: @sume
  */
@@ -14,6 +14,7 @@
 int add(int num1, int num2)
 {
 	int sum;
+
 	sum = num1 + num2;
 	return (sum);
 }
