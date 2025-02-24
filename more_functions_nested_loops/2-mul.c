b72e5311291db26967c7de3e8439985081070184
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-24 12:01:32 +0100

    Task: 2.collaboration is multiplication, File:2-mul.c, the function multiply two variables

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul -function that do the multiplication
+ *
+ * @a: int, first number
+ * @b: int, scd number
+ *
+ * Return: int, 
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
