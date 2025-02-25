8e5ad1a4f5609b9926251b59ae135dda77eec667
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-25 11:13:30 +0100

    Task:9.Fizz-Buzz, File:9-fizz_buzz.c , print number 1-100 and replace number with Fizz and Buzz or FizzBuzz if the numbers are completly divided by 3 and 5 or only one of them

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main -entry level
+ * Return: int, zero
+ * number -int,  from 1 to 100
+ * mod3 -int, model of 3
+ * mod5 -int, model of 5
+ *
+ */
+
+int main(void)
+{
+	int number;
+	int mod3;
+	int mod5;
+
+	for (number = 1; number <= 100; number++)
+	{
+		mod3 = number % 3;
+		mod5 = number % 5;
+		if (mod3 == 0 && mod5 != 0)
+		{
+			printf("Fizz ");
+		}
+		if (mod5 == 0 && mod3 != 0)
+		{
+			printf("Buzz ");
+		}
+		if (mod3 == 0 && mod5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (mod3 != 0 && mod5 != 0)
+		{
+			printf("%d ", number);
+		}
+	}
+	printf("\n");
+	return (0);
+}
