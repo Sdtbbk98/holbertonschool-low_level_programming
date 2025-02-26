5fce28b9a31504e9edf83df0aaa655677c556098
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-26 16:17:14 +0100

    Task:5, File:5-rev_string.c Change: added strlen function code

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,10 +9,16 @@
 void rev_string(char *s)
 {
 	int index = 0;
-	int lenght = _strlen(s) - 1;
+	int i;
+	int lenght;
 	/* -1 to start form 0*/
 	char chr;
 
+	for (i = 0; s[i]; i++)
+	{
+		lenght = i + 1;
+	}
+	lenght = lenght - 1;
 	while (index < lenght)
 	{
 		chr = s[index];
