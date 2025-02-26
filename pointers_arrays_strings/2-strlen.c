eaa9e5dce6acfe20d1862825ce0822d4f1a9e14f
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-26 11:06:11 +0100

    folder name error correction

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function that calculate length
+ * @s: , char pointer
+ * Return: int, length of @*s
+ */
+
+int _strlen(char *s)
+{
+	int length;
+	int index = 0;
+
+	for (; s[index]; index++)
+	{
+		length = index + 1;
+		/**return (length);*/
+	}
+	return (length);
+}
