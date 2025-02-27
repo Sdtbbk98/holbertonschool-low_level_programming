be119a1850c57fffbf6110ee9a02d1cde638d1e7
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-27 23:22:42 +0100

    Task: 6. Half the lies they tell about me arent true, File: 6-puts2.c, changes: added a loop that let us mesure the lenght

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,28 @@
 #include "main.h"
-
 /**
  * puts2 - function that print pair characters
- * @str: char,
- * Return: nada
- * index - int need for itération
+ * @str: char
  */
 
 void puts2(char *str)
 {
-	int index;
+	int index = 0;
+	int index1;
+	int lenght;
 
-	for (index = 0; str[index] != '\0'; index += 2)
+	for (index1 = 0; str[index1]; index1++)
 	{
-		_putchar(str[index]);
+		lenght = index1 + 1;
+	}
+
+	for ( ; index < lenght; index++)
+	{
+		if (index % 2 == 0)
+		{
+			_putchar(str[index]);
+		}
 	}
 	_putchar('\n');
 
 }
+
