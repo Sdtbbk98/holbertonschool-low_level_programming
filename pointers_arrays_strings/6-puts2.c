1ed6f8021c0b27f7904c6a93cf03f9ef2d9cbace
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-27 21:59:50 +0100

    Task: 6. Half the lies they tell about me arent true, File: 6-puts2.c, add betty documentation and correction

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- *
- *
+ * puts2 - function that print pair characters
+ * @str: char,
+ * Return: nada
+ * index - int need for itération
  */
 
 void puts2(char *str)
 {
 	int index;
 
-	for (index = 0;str[index] != '\0'; index += 2)
+	for (index = 0; str[index] != '\0'; index += 2)
 	{
 		_putchar(str[index]);
 	}
