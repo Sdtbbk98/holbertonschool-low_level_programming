9206608adc4de28878077caf738eed8dca466d90
sdtbbk98 <sadat.babak2@gmail.com>
2025-02-28 13:51:34 +0100

    Task:9.strcpy, file: 9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - function, that copy str pointed by @src include th \0
+ * @src: char, string source
+ * @dest:char, our buffer
+ * Return: char, @dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		/*printf("%d", src[index]);*/
+		index++;
+	}
+	return (dest);
+
+}
