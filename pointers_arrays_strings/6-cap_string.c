99e7fcea4e5d66a37c0412e5a946d94d050a8f69
sdtbbk98 <sadat.babak2@gmail.com>
2025-03-02 21:07:06 +0100

    Task:  6. Expect the best. Prepare for the worst. Capitalize on what comes, File:6-cap.string.c, function that captalizes all the words.

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* *cap_string - function that capitalizes all words
+* @str: char *, strings given
+* Return: char @str:
+* n_word: int, new_word not n.g.a word it determine our first character.
+*/
+
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	int n_word = 1;
+
+	while (*ptr != '\0')
+	{
+		if ((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z'))
+		{
+			if (n_word && *ptr >= 'a' && *ptr <= 'z')
+			{
+				*ptr -= ('a' - 'A');
+			}
+			n_word = 0;
+		}
+		else
+		{
+			if (*ptr == ',' || *ptr == ';' || *ptr == '!' || *ptr == '?' ||
+				*ptr == '.' || *ptr == '"' || *ptr == '(' || *ptr == ')' ||
+				*ptr == '{' || *ptr == '}' || *ptr == ' ' || *ptr == '\n' ||
+				*ptr == '\t') {
+			}
+			n_word = 1;
+		}
+		ptr++;
+	}
+	return (str);
+}
