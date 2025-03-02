7a3107d5e07e6d04088120ddddc4912bc4054838
sdtbbk98 <sadat.babak2@gmail.com>
2025-03-02 15:20:58 +0100

    changes: in first loop i copy the n character from src to dest so i should look the charater in src!!! and in secound loop i add null character if my src is shorter than n normely it shouldnt be the case

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0;
 
-	while (index < n && dest[index] != '\0')
+	while (index < n && src[index] != '\0')
 	{
 		dest[index] = src[index];
 		index++;
