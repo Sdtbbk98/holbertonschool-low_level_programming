36a7e4faf409db0bf5af6ecf59c6907858c62536
sdtbbk98 <sadat.babak2@gmail.com>
2025-03-02 21:58:36 +0100

    make mistake in task describtion i need to use only one if

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,32 +8,26 @@
 
 char *leet(char *str)
 {
-
-	char *Pstr = str;
+	char *Main = str;
+	char *characters = "aeotlAEOTL";
+	char *replacements = "4307143071";
 
 	while (*str)
 	{
-		if (*str == 'a' || *str == 'A')
-		{
-			*str = '4';
-		}
-		if (*str == 'e' || *str == 'E')
-		{
-			*str = '3';
-		}
-		if (*str == 'o' || *str == 'O')
-		{
-			*str = '0';
-		}
-		if (*str == 't' || *str == 'T')
-		{
-			*str = '7';
-		}
-		if (*str == 'l' || *str == 'L')
-		{
-			*str = '1';
+		char *leet_ptr = characters;
+		char *replacement_ptr = replacements;
+
+		while (*leet_ptr)
+	{
+			if (*str == *leet_ptr)
+			{
+				*str = *replacement_ptr;
+				break;
+			}
+			leet_ptr++;
+			replacement_ptr++;
 		}
 		str++;
 	}
-	return (Pstr);
+	return (Main);
 }
