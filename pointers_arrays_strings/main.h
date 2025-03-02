4a7183e028e7441e30ecbc1db88cfec5cb008a38
sdtbbk98 <sadat.babak2@gmail.com>
2025-03-02 13:56:13 +0100

    Project 2(more pointers, arrays and strings) were added to main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*project 1 prototype (pointer arrays and strings)*/
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
@@ -25,5 +26,22 @@ char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
 
+/*project 2 prototypes (more poiners and array )*/
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
 
 #endif
