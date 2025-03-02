7171fc35b358a14eded16b82ae736a4ef3e5d890
sdtbbk98 <sadat.babak2@gmail.com>
2025-03-02 06:34:10 +0100

    Task:10 , file:100-atoi.c , it convert string to int

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -5,24 +5,18 @@
  * @s: Pointer to the string to convert.
  * Return: The converted integer value.
  */
-
 int _atoi(char *s)
 {
 	int index = 0;
 	int result = 0;
 	int sign = 1;
 
-	while (s[index] == ' ')
+	while (s[index] != '\0' && !(s[index] >= '0' && s[index] <= '9'))
 	{
-		index++;
-	}
-	if (s[index] == '+')
-	{
-		index++;
-	}
-	else if (s[index] == '-')
-	{
-		sign = -1;
+		if (s[index] == '-')
+		{
+			sign = -sign;
+		}
 		index++;
 	}
 	while (s[index] >= '0' && s[index] <= '9')
