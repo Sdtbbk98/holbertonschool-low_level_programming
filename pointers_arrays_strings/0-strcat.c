5c07ba16f7d454dcb257ae25c8400e424a41de35
sdtbbk98 <sadat.babak2@gmail.com>
2025-03-02 14:12:07 +0100

    Task:0.strcat, file: 0-strcat.c , the prototype concatenates two stirngs, changes: add iteration by number befor it was by chracter so now i use array not pointer.

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -4,22 +4,25 @@
 * *_strcat - function that do the concatenation of 2 strings.
 * @dest: char *
 * @src: char *
+* len_dest - lenght of destination int
+* len_src - lenght of source int
 * Return: the result of conatenation
 */
 
 char *_strcat(char *dest, char *src)
 {
+	int len_dest = 0;
+	int len_src = 0;
 
-	while (*dest != '\0')
+	while (dest[len_dest] != '\0')
 	{
-		dest++;
+		len_dest++;
 	}
-	while (*src != '\0')
+	while (src[len_src] != '\0')
 	{
-		*dest = *src;
-		dest++;
-	src++;
+		dest[len_dest + len_src] = src[len_src];
+		len_src++;
 	}
-	*dest = '\0';
+	dest[len_dest + len_src] = '\0';
 	return (dest);
 }
