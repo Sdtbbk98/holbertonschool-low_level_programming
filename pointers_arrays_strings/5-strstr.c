a58407c8cc95cc920db83aae5319727798b4b355
sdtbbk98 <sadat.babak2@gmail.com>
2025-03-05 07:39:39 +0100

    Tast: 5.strstr, File:3-strstr.c, function that locate substrings in main string

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* _strstr - function that locate substrings
+* @haystack: char*, buffer
+* @needle: char*, substring
+* Return: char*, located substring or NULL if not find
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *start_haystack = haystack;
+	
+	/*checking every character in haystack */
+	while (*haystack != '\0')
+	{
+		char *pt_haystack = haystack;
+		char *pt_needle = needle;
+
+		/*checking for match btw haystack and needle*/
+		while (*pt_haystack == *pt_needle && *pt_needle != '\0')
+		{
+			pt_haystack++;
+			pt_needle++;
+		}
+		/*by reaching the end of needle, if we found a match*/
+		if (*pt_needle == '\0')
+		{
+			return (start_haystack);
+		}
+		haystack++;
+		/*adding the next character in the haystack*/
+		start_haystack++;
+	}
+	return (0);
+}
+
